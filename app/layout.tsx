5d3d2d3d348523ec3cac2bcb530180cf81024b2e
Nick Carnevale <ncarnevale12@gmail.com>
2026-05-08 17:34:03 -0500

    increase default font size

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${specialElite.variable} min-h-dvh antialiased`}>
-      <body className="flex min-h-dvh flex-col">{children}</body>
+      <body className="flex min-h-dvh flex-col text-base">{children}</body>
     </html>
   );
 }
