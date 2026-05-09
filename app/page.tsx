a979a609ca348f1101122af9c7b7140e6bbc9dde
Nick Carnevale <ncarnevale12@gmail.com>
2026-05-08 21:14:29 -0500

    warning for mobile users

--- app/page.tsx
+++ app/page.tsx
@@ -9,11 +9,14 @@ export default async function Home() {
   return (
     <main className="flex min-h-0 flex-1 flex-col items-start justify-start px-4 pt-10 pb-0 sm:px-8 sm:pt-16 sm:pb-0">
       <div className="mb-12 font-bold">
-        They say that given enough time, a monkey in a room with a typewriter
-        would produce the complete works of William Shakespeare. You are that
-        monkey.
+        <span className="block sm:hidden">Monkeys don&apos;t use smartphones!</span>
+        <span className="hidden sm:block">
+          They say that given enough time, a monkey in a room with a typewriter
+          would produce the complete works of William Shakespeare. You are that
+          monkey.
+        </span>
       </div>
-      <article className="page typed flex w-full max-w-4xl mx-auto flex-1 flex-col px-8 py-8 text-left text-base leading-7 sm:px-16">
+      <article className="page typed hidden w-full max-w-4xl mx-auto flex-1 flex-col px-8 py-8 text-left text-base leading-7 sm:flex sm:px-16">
         <TypewriterPage text={text} />
       </article>
     </main>
